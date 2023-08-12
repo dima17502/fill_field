24f338f164002471908ea03296fd01a722b0345a
dima17502 <53102183+dima17502@users.noreply.github.com>
2023-08-12 14:58:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-onkeydown = e => e.key == " " ? clicksCountSpan.innerText++:0
+/*
+    TODO
+    поле
+
+
+*/
+
+var game_field_size = 800;
+
+
+function main()
+{
+    set_body();
+    create_game_field();
+    create_info_field();
+
+}
+
+function set_body()
+{
+    document.body.background = "url('images/back.jpg')";
+
+}
+function create_game_field()
+{
+    elem = document.createElement("div");
+    elem.id = "gameField";
+    elem.style.position = "absolute";
+    elem.style.width = game_field_size + "px";
+    elem.style.height = game_field_size + "px";
+    elem.style.marginLeft = "600px";
+    elem.style.marginTop = "50px";
+    elem.style.border = "3px solid black";
+    document.body.appendChild(elem);
+}
