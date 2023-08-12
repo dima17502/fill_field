950a0232a596b78dd3679d58af6d8a6148fa62b5
dima17502 <53102183+dima17502@users.noreply.github.com>
2023-08-12 16:00:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,9 @@
 
 */
 
-var game_field_size = 700;
-
+var game_field_size = 600;
+var vk_width = 1000;
+var vk_height = 750;
 
 function main()
 {
@@ -23,14 +24,21 @@ function set_body()
 }
 function create_game_field()
 {
+    eframe = document.createElement("div");
+    eframe.id = "window";
+    eframe.position = "absolute";
+    eframe.style.border = "1px solid red";
+    eframe.style.width = vk_width + "px";
+    eframe.style.height = vk_height + "px";
+    document.body.appendChild(eframe);
     elem = document.createElement("div");
     elem.id = "gameField";
     elem.style.position = "absolute";
     elem.style.width = game_field_size + "px";
     elem.style.height = game_field_size + "px";
-    elem.style.marginLeft = "100px";
+    elem.style.marginLeft = "10%";
     elem.style.marginTop = "50px";
     elem.style.border = "3px solid black";
     elem.style.background = "black";
-    document.body.appendChild(elem);
+    eframe.appendChild(elem);
 }
